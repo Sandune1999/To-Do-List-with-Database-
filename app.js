3289919e6ab8271db4fdaa2b7e9fe21abbc4a096
Sandip Mandal <sandip92809mandal@gmail.com>
2022-06-13 17:52:34 +0530

    added gitignore file,procfile and upfup

--- app.js
+++ app.js
@@ -107,6 +107,15 @@ app.post("/", function(req, res){
       res.redirect("/" + listName);
     });
   }
+
+  //without mongoose
+// if (req.body.list === "Work") {
+//   workItems.push(item);
+//   res.redirect("/work");
+// } else {
+//   items.push(item);
+//   res.redirect("/");
+// }
 });
 
 app.post("/delete", function(req, res){
@@ -131,10 +140,25 @@ app.post("/delete", function(req, res){
 
 });
 
+
+// app.get("/work", function(req, res) {
+//   res.render("list", {
+//     listTitle: "Work List",
+//     newListItems: workItems
+//   });
+// });
+
+
+
 app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server has started Successfully");
 });
